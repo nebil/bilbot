5516f6df4741d20c50b471040ce34c866118740e
Nebil Kawas <nkawas90@gmail.com>
2016-09-30 22:04:41 -0300

    :seedling: Added a first basic command
    This command will be used to kick-start the bot.

--- bilbot.py
+++ bilbot.py
@@ -6,10 +6,21 @@ This source code is subject to the terms of the Mozilla Public License.
 You can obtain a copy of the MPL at <https://www.mozilla.org/MPL/2.0/>.
 """
 
+from telegram.ext import CommandHandler, Updater
+
+
+def start_command(bot, update):
+    update.message.reply_text("Bilbot, operativo.")
+
 with open('bilbot.cfg') as cfgfile:
     CONFIG_DICT = dict(line.rstrip().split('=') for line in cfgfile)
     TGBOT_TOKEN = CONFIG_DICT.get('bot_token')
-    print(TGBOT_TOKEN)
+
     if not TGBOT_TOKEN:
         raise Exception("\nThe bot token is missing."
                         "\nDeclare the token in the configuration file.")
+
+updater = Updater(token=TGBOT_TOKEN)
+updater.dispatcher.add_handler(CommandHandler('start', start_command))
+updater.start_polling()
+updater.idle()
