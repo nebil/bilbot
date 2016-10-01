6090d36d2812bc71c5d4534285ec64627337e5b5
Nebil Kawas <nkawas90@gmail.com>
2016-09-30 22:18:08 -0300

    :new: Added two new commands: /about and /help

--- bilbot.py
+++ bilbot.py
@@ -9,8 +9,21 @@ You can obtain a copy of the MPL at <https://www.mozilla.org/MPL/2.0/>.
 from telegram.ext import CommandHandler, Updater
 
 
+# COMMANDS
+# ========
+
 def start_command(bot, update):
     update.message.reply_text("Bilbot, operativo.")
+
+
+def about_command(bot, update):
+    update.message.reply_text("Hola, mi nombre es Nebilbot.")
+    update.message.reply_text("Pero también me puedes llamar Bilbot.")
+
+
+def help_command(bot, update):
+    update.message.reply_text("Mis comandos son: `/about`, `/help`, `/start`.",
+                              parse_mode='markdown')
 
 with open('bilbot.cfg') as cfgfile:
     CONFIG_DICT = dict(line.rstrip().split('=') for line in cfgfile)
@@ -22,5 +35,8 @@ with open('bilbot.cfg') as cfgfile:
 
 updater = Updater(token=TGBOT_TOKEN)
 updater.dispatcher.add_handler(CommandHandler('start', start_command))
+updater.dispatcher.add_handler(CommandHandler('about', about_command))
+updater.dispatcher.add_handler(CommandHandler('help',  help_command))
+
 updater.start_polling()
 updater.idle()
